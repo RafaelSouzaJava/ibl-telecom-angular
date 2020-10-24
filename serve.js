d1464aaaa960da01b032f53df7770448bdbf6550
RafaelSouzaJava <rafael-souza4@outlook.com>
2020-10-24 02:23:18 -0300

    publicando no heroku

--- serve.js
+++ serve.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/dist'));
 
-app.use(express.static(__dirname + '/dist/IBLTelecom'));
-
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/IBLTelecom/index.html');
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(PORT, () => {
-  console.log('Servidor iniciado na porta ' + PORT);
-})
+app.listen(process.env.PORT || 4200);
