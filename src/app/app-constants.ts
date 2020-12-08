e0a57d7a51ba8cd7eb1ff9c39ef25383be6e7627
RafaelSouzaJava <rafael-souza4@outlook.com>
2020-12-07 21:53:09 -0300

    enviando projeto

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-    public static get baseServidor(): string {return "http://localhost:8080/"}
+    public static get baseServidor(): string {return 'https://rafael-ibl.herokuapp.com/'}
 
     public static get baseLogin(): string {return this.baseServidor + 'login'}
 
